b1eaa4c6d01a13bcaffc8f74bd3b9ff103097020
Matheus Mendes <50635097+matheusfmendes@users.noreply.github.com>
2020-08-17 17:22:31 -0300

    Create spin.py

---
+++ spin.py
@@ -0,0 +1,12 @@
+from random import choices
+print ("Ola! escolha um  numero entre: 1 e 6")
+
+n1 = str(input('Primeiro numero:'))
+n2 = str(input('Segundo numero:'))
+n3 = str(input('Terceiro numero:'))
+n4 = str(input('Quarto numero:'))
+n5 = str(input('Quinto numero:'))
+n6 = str(input('Sexto numero:'))
+lista [n1,n2,n3,n4,n5,n6]
+escolido = random.choices(lista)
+print("O numero escolhido foi {}".format(escolhido))
